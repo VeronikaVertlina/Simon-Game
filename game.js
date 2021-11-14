e46fe78b4d8b07d18ad0903d8a671edf5a07aec1
ronya78@gmail.com <ronya78@gmail.com>
2021-11-14 20:11:29 +0200

    Add code

--- game.js
+++ game.js
@@ -3,33 +3,57 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
+var userClickedPattern = [];
+
+//1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
+//2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+//--> So if the Green button was clicked, userChosenColour will equal its id which is "green".
+$('.btn').click(function() {
+
+  var userChosenColour = this.id; // var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  // console.log(userClickedPattern);
+});
+
+
+
+
 function nextSequence() {
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  alert(randomChosenColour);
-
   //1. Use jQuery to select the button with the same id as the randomChosenColour
   //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
-  $('#' + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100).click(function(){
+  $('#' + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  //-------Step 3 - Show the Sequence to the User with Animations and Sounds
-  //1. Use jQuery to select the button with the same id as the randomChosenColour
-  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
-  // Hint 1. Remember that to select ids with jQuery you need the "#" symbol.
-  // https://api.jquery.com/id-selector/
-  // Hint 2. https://www.google.co.uk/search?q=animate+flash+jQuery
-  // Hint 3. https://www.google.co.uk/search?q=play+sound+javascript
+  //4. Refactor the code in playSound() so that it will work for both playing sound
+  //in nextSequence() and when the user clicks a button.
+  playSound(randomChosenColour);
 
-
-    var audio = new Audio ('sounds/' + randomChosenColour + '.mp3');
-    audio.play();
-  });
 }
 
+//2. Create a new function called playSound() that takes a single input parameter called name.
+function playSound(name) {
+
+  //Take the code we used to play sound in the nextSequence()
+  //function and move it to playSound().
+  var audio = new Audio ('sounds/' + name + '.mp3');
+  audio.play();
+}
+
+
 nextSequence();
+
+
+
+
+
+
+
 
 // var buttonCollours  = ["red", "blue", "green", "yellow"];
 //
