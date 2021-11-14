d544b268a1d7299c83b14c9c7230d6ca8e8a42cd
ronya78@gmail.com <ronya78@gmail.com>
2021-11-14 17:29:59 +0200

    Add code to game.js

--- game.js
+++ game.js
@@ -1,44 +1,91 @@
-//3. At the top of the game.js file, create a new array called buttonColours
-var buttonCollours  = ["red", "blue", "green", "yellow"];
 
-//5. At the top of the game.js file, create a new empty array called gamePattern.
+var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 
-//1. Inside game.js create a new function called nextSequence()
 function nextSequence() {
-  //2. Inside the new function generate a new random number between 0 and 3,
-  //and store it in a variable called randomNumber
-  var randomNumber = Math.floor((Math.random() * 4));
 
-  //4. Create a new variable called randomChosenColour and use the randomNumber from step 2
-  //to select a random colour from the buttonColours array.
-  var randomChosenColour = buttonCollours[randomNumber];
-
-  //6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  // alert(randomChosenColour);
-  selectBtn(randomChosenColour);
+  alert(randomChosenColour);
 
-}
+  //1. Use jQuery to select the button with the same id as the randomChosenColour
+  //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
+  $('#' + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100).click(function(){
+
+  //-------Step 3 - Show the Sequence to the User with Animations and Sounds
+  //1. Use jQuery to select the button with the same id as the randomChosenColour
+  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+  // Hint 1. Remember that to select ids with jQuery you need the "#" symbol.
+  // https://api.jquery.com/id-selector/
+  // Hint 2. https://www.google.co.uk/search?q=animate+flash+jQuery
+  // Hint 3. https://www.google.co.uk/search?q=play+sound+javascript
 
 
-//-------Step 3 - Show the Sequence to the User with Animations and Sounds
-//1. Use jQuery to select the button with the same id as the randomChosenColour
-//3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
-function selectBtn(randomChosenColour) {
-  if(randomChosenColour === "yellow"){
-    $("#yellow").fadeOut(100).fadeIn(100);
-  }
-  else if (randomChosenColour === "green"){
-    $("#green").fadeOut(100).fadeIn(100);
-  }
-  else if (randomChosenColour === "red"){
-    $("#red").fadeOut(100).fadeIn(100);
-  }
-  else {
-    $("#blue").fadeOut(100).fadeIn(100);
-  }
+    var audio = new Audio ('sounds/' + randomChosenColour + '.mp3');
+    audio.play();
+  });
 }
 
 nextSequence();
+
+// var buttonCollours  = ["red", "blue", "green", "yellow"];
+//
+// var gamePattern = [];
+//
+// function nextSequence() {
+//   var randomNumber = Math.floor((Math.random() * 4));
+//   var randomChosenColour = buttonCollours[randomNumber];
+//   gamePattern.push(randomChosenColour);
+//
+//   // alert(randomChosenColour);
+//   selectBtn(randomChosenColour);
+//
+// }
+//
+//
+// //-------Step 3 - Show the Sequence to the User with Animations and Sounds
+// //1. Use jQuery to select the button with the same id as the randomChosenColour
+// //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+// // Hint 1. Remember that to select ids with jQuery you need the "#" symbol.
+// // https://api.jquery.com/id-selector/
+// // Hint 2. https://www.google.co.uk/search?q=animate+flash+jQuery
+// // Hint 3. https://www.google.co.uk/search?q=play+sound+javascript
+//
+//   function selectBtn(randomChosenColour) {
+//   switch(randomChosenColour){
+//     case "yellow":
+//     $("#yellow").fadeOut(100).fadeIn(100).click(function() {
+//       var yellowAudio = new Audio ('sounds/yellow.mp3');
+//       yellowAudio.play();
+//     });
+//     break;
+//
+//     case "green":
+//     $("#green").fadeOut(100).fadeIn(100).click(function(){
+//       var greenAudio = new Audio ('sounds/green.mp3');
+//       greenAudio.play();
+//     });
+//     break;
+//
+//     case "red":
+//     $("#red").fadeOut(100).fadeIn(100).click(function(){
+//       var redAudio = new Audio ('sounds/red.mp3');
+//       redAudio.play();
+//     });
+//     break;
+//
+//     case "blue":
+//     $("#blue").fadeOut(100).fadeIn(100).click(function(){
+//       var blueAudio = new Audio ('sounds/blue.mp3');
+//       blueAudio.play();
+//     });
+//     break;
+//
+//     default: console.log(nextSequence());
+//   }
+// }
+//
+// nextSequence();
