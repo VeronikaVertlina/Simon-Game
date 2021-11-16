b2b5214a2beab3dc0274c4fdbd1b3713118418a9
ronya78@gmail.com <ronya78@gmail.com>
2021-11-16 19:57:58 +0200

    add code

--- game.js
+++ game.js
@@ -7,13 +7,11 @@ var userClickedPattern = [];
 var level = 0;
 var start = false;
 
-//1. Use jQuery to detect when a keyboard key has been pressed,
-//when that happens for the first time, call nextSequence().
+//1. Use jQuery to detect when a keyboard key has been pressed,when that happens for the first time, call nextSequence().
 $(document).keypress(function(){
   // alert("A key was pressed");
     if(!start){
-    //3. The h1 title starts out saying "Press A Key to Start",
-    //when the game has started, change this to say "Level 0".
+    //3. The h1 title starts out saying "Press A Key to Start",when the game has started, change this to say "Level 0".
     $("#level-title").text("Level " + level);
     nextSequence();
     start = true;
@@ -27,8 +25,10 @@ $('.btn').click(function() {
   var userChosenColour = $(this).attr("id"); // var userChosenColour = this.id;
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
-  // console.log(userClickedPattern);
+
   animatePress(userChosenColour);
+  console.log("User pattern is " + userClickedPattern);
+  checkAnswer(level);
 });
 
 
@@ -39,34 +39,52 @@ function nextSequence() {
   //5. Inside nextSequence(), update the h1 with this change in the value of level.
   $("#level-title").text("Level " + level);
 
-
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   //1. Use jQuery to select the button with the same id as the randomChosenColour
-  //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
   $('#' + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  //4. Refactor the code in playSound() so that it will work for both playing sound
-  //in nextSequence() and when the user clicks a button.
+  //4. Refactor the code in playSound() so that it will work for both playing sound in nextSequence() and when the user clicks a button.
   playSound(randomChosenColour);
+  console.log("Game pattern is " + gamePattern);
+
+
 }
+
+
+
+//1. Create a new function called checkAnswer(), it should take one input with the name currentLevel
+function checkAnswer(currentLevel) {
+  //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+  if(userClickedPattern.every(function(element){
+    gamePattern.includes(element);
+  })
+    console.log("success");)
+  //{
+  //    currentLevel++;
+  //    $("#level-title").text("Level " + currentLevel);
+  //     console.log("success");
+  //     nextSequence();
+  //   }
+    // else {
+    //   alert("wrong");
+    // }
+}
+
+
 
 //2. Create a new function called playSound() that takes a single input parameter called name.
 function playSound(name) {
-
-  //Take the code we used to play sound in the nextSequence()
-  //function and move it to playSound().
+  //Take the code we used to play sound in the nextSequence() function and move it to playSound().
   var audio = new Audio ('sounds/' + name + '.mp3');
   audio.play();
 }
 
 //1. Create a new function called animatePress(), it should take a single input parameter called currentColour.
 function animatePress(currentColour) {
-
-  //2. Take a look inside the styles.css file, you can see there is a class called "pressed",
-  //it will add a box shadow and changes the background colour to grey
+  //2. Take a look inside the styles.css file, you can see there is a class called "pressed", it will add a box shadow and changes the background colour to grey
   //3. Use jQuery to add this pressed class to the button that gets clicked inside animatePress().
     var activeButton = $("#" + currentColour);
     activeButton.addClass("pressed");
